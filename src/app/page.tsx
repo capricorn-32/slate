01ca90eb7c09614e17c886898b3824be9d4bc78b
seniorliketocode <abhishek234543@gmail.com>
2025-04-14 19:50:38 +0530

    editor added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <Button>Touch me</Button>
+      Click <Link href="/documents">here</Link>
     </div>
   );
 };
