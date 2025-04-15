7a2b3a9c0c16677fc29f4d39902ebe59092b99c4
seniorliketocode <abhishek234543@gmail.com>
2025-04-16 04:07:31 +0530

    fontsize extension added

---
+++ src/app/documents/font-size.ts
@@ -0,0 +1,61 @@
+import { Extension } from "@tiptap/react";
+import "@tiptap/extension-text-style";
+
+declare module "@tiptap/core" {
+  interface Commands<ReturnType> {
+    fontSize: {
+      setFontSize: (size: string) => ReturnType;
+      unsetFontSize: () => ReturnType;
+    };
+  }
+}
+
+export const FontSizeExtensize = Extension.create({
+  name: "fontsize",
+  addOptions() {
+    return {
+      types: ["textStyles"],
+    };
+  },
+  addGlobalAttributes() {
+    return [
+      {
+        types: this.options.types,
+        attributes: {
+          fontSize: {
+            default: null,
+            parseHTML: (element) => element.style.fontSize,
+            renderHTML: (attributes) => {
+              if (!attributes.fontsize) {
+                return {};
+              }
+
+              return {
+                style: `font-size: ${attributes.fontsize}`,
+              };
+            },
+          },
+        },
+      },
+    ];
+  },
+
+  addCommands() {
+    return {
+      setFontSize:
+        (fontSize: string) =>
+        ({ chain }) => {
+          return chain().setMark("textStyle", { fontSize }).run();
+        },
+
+      unsetFontSize:
+        () =>
+        ({ chain }) => {
+          return chain()
+            .setMark("textStyle", { fontSize: null })
+            .removeEmptyTextStyle()
+            .run();
+        },
+    };
+  },
+});
